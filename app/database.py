ec54cdac3da8061a34734a07dab347fd6cbe3cba
Sadha M <amisadha0519@gmail.com>
2025-01-21 06:26:22 +0530

    Debugged database file issue

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,3 +11,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Add this function - this was the missing piece!
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
